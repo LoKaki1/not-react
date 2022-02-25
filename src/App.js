6760ffbbe50f47fa0be8d4651febd70d15538bfd
LoKaki1 <91605169+LoKaki1@users.noreply.github.com>
2022-02-25 09:56:45 +0200

    Brendon fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+    const [brendon, setBrendon] = useState([])
+    const brendonClick = () => {
+      axios.post('http://127.0.0.1:5000/predict', {
+        ticker: 'NIO'
+      })
+      .then((response) => {
+        console.log(response)
+        setBrendon(response.data);
+      }, (error) => {
+        console.log(error);
+      });
+    }
+    return (
+      <>
+    <div>
+      <button className='btn' onClick={() => brendonClick()}>Brendon..</button>
     </div>
-  );
-}
+    <div>
+      <p>{brendon}</p>
+    </div>
+    </>
+    )
+  }
 
-export default App;
+
