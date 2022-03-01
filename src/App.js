9270777d27337c24a4b204327d40a7f54244f1e9
LoKaki1 <91605169+LoKaki1@users.noreply.github.com>
2022-03-01 11:46:06 +0200

    Brendon Commit thanks :)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 export default function App() {
     const [brendon, setBrendon] = useState([])
+    const [benny, setBenny] = useState({
+      
+      
+    }) 
+
     const brendonClick = () => {
-      axios.post('http://127.0.0.1:5000/predict', {
-        ticker: 'NIO'
-      })
+      axios.post('http://127.0.0.1:5000/predict', benny || {"ticker": 'NIO'})
       .then((response) => {
         console.log(response)
-        setBrendon(response.data);
+        setBrendon((oldList) =>
+        {
+          let newList = JSON.parse(JSON.stringify(oldList))
+          newList.push(response.data)
+          return  newList
+        })  
       }, (error) => {
         console.log(error);
-      });
+      })
+      
     }
+
+    const handleChange = (e, key) => {
+        //SetShahar(gay=true)
+        setBenny((oldBenny) => {
+          return {   
+          ... oldBenny,
+          key: e.target.value
+        }
+        })
+        
+      }
+  
+    
+
     return (
       <>
     <div>
       <button className='btn' onClick={() => brendonClick()}>Brendon..</button>
     </div>
     <div>
-      <p>{brendon}</p>
+      {brendon.map((item, index) => {
+        return <p key={index}> {item}</p>
+      })}
     </div>
+ 
+    <input type="text"  onChange={(e) => handleChange(e, 'ticker')} />
+    {/* <input type="text"  onChange={handleChange()} /> */}
     </>
     )
+  
   }
-
 
